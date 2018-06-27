9add4c7208bec4e0ad9c4bf357b292c4c5f4d0e3
myungbeans <michael.h.myung@gmail.com>
2018-06-27 16:03:42 -0400

    Added Material Icons to project. Custom Icons for increasing and decreasing Increment

--- src/containers/CustomMenuContainer.js
+++ src/containers/CustomMenuContainer.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField'
+// import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
 
 import { connect } from 'react-redux'
 import { updateGridSettings } from '../actions/settings-actions'
 import { bindActionCreators } from 'redux'
 
 class CustomMenuContainer extends Component {
-    localState = {
-        error: false,
-        label: "Graph Interval"
-    }
+    // localState = {
+    //     error: false,
+    //     label: "Graph Interval"
+    // }
 
     onGridSettingsChange = (event) => {
-        if (!parseInt(event.target.value)) {
-            this.setState({error: true, label: "Input must be an Integer"})
-        } else {
-            this.setState({errorText: '', label: "Graph Interval"})
-            console.log(event.target)
-            const interval = event.target.value
-            this.props.updateGridSettings({ interval })
-        }
+        this.setState({errorText: '', label: "Graph Interval"})
+        console.log(event.target)
+        const interval = event.target.value
+        this.props.updateGridSettings({ interval })
     }
 
     render() {
+        console.log(this.props)
         return (
-            <TextField onChange={this.onGridSettingsChange} name="interval" id="interval" label={this.localState.label} error={this.localState.error} value={this.props.interval} />
+            <div>
+                {/* <TextField onChange={this.onGridSettingsChange} name="interval" id="interval" label={this.localState.label} error={this.localState.error} value={this.props.interval} /> */}
+                <IconButton color="default"><ExpandLess/></IconButton>
+                {this.props.settings.interval}
+                <IconButton color="default"><ExpandMore/></IconButton>
+            </div>
         )
     }
 }
