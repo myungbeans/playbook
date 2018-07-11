03408179a0fdd8e68207bebc9187aa41adc5a3af
myungbeans <michael.h.myung@gmail.com>
2018-07-11 13:20:26 -0400

    Added error handling messages to create user page

--- src/containers/NewUser.js
+++ src/containers/NewUser.js
@@ -7,6 +7,8 @@ import { connect } from 'react-redux'
 import { routeActions } from 'react-router-redux'
 import { withRouter } from 'react-router-dom'
 
+import { toggleError, errorMsg } from '../actions/settings-actions'
+
 class NewUser extends Component {
     state = {
         toggle: true,
@@ -25,11 +27,13 @@ class NewUser extends Component {
         })
         .then(res => res.json())
         .then(data => {
-            //TODO: Check for error messages on the response
-            //TODO: fadeout effect
-            // localStorage.setItem('token', data.token)
-            // localStorage.setItem('id', data.id)
-            this.props.history.push('/login')
+            if (data.errors){
+                console.log(data)
+                this.props.errorMsg(`${data.errors[0]}`)
+                this.props.toggleError(true)
+                return null
+            }
+            this.redirectToLogin()
         })
     }
 
@@ -38,7 +42,6 @@ class NewUser extends Component {
     }
 
     render() {
-        console.log(this.props)
         return (
             <div className='outer-div-create-user'>
                 <Fade in={this.state.toggle} timeout={this.state.inOut} >
@@ -65,7 +68,7 @@ const mapStateToProps = state => {
 
 const mapActionsToProps = (dispatch) => {
     return bindActionCreators({
-        ...routeActions
+        ...routeActions, toggleError, errorMsg
     }, dispatch)
 }
   
